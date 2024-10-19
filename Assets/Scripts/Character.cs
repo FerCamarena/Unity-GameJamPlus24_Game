e8277755f789fdfbef7d35a99046542c6b1f275d
FerCamarena <fernando.camarena6817@alumnos.udg.mx>
2024-10-19 02:13:41 -0600

    Base 3D movement
    Remplazed prefab for default character with default 3D character.
    Added movement from inputs to default 3D character.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+    public _InputManager _Input;
+
+    private void Start() {
+        if (!_Input) _Input = GameObject.Find("UI").GetComponent<_InputManager>();
+    }
+
+    private void Update() {
+        Move();
+    }
+
+    private void Move() {
+        Vector3 move = Vector3.zero;
+        if (_Input.A_State) {
+            move += Vector3.left;
+        }
+        if (_Input.D_State) {
+            move += Vector3.right;
+        }
+        if (_Input.W_State) {
+            move += Vector3.forward;
+        }
+        if (_Input.S_State) {
+            move += Vector3.back;
+        }
+
+        transform.Translate(move.normalized * 4 * Time.deltaTime, Space.Self);
+    }
+}
